9e6b88db0563f8619b328b1073b78da13dbf9acf
uzapolsky <uzapolsky@gmail.com>
2021-09-16 17:14:57 +0300

    add getting places json info at /places/<id> pages

--- places/views.py
+++ places/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import response
+from django.shortcuts import render, get_object_or_404
+from django.http.response import JsonResponse
+from .models import Place, Image
 
-# Create your views here.
+def place(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    images = place.images.all().order_by('position')
+    response = {
+        'title': place.title,
+        'imgs': [image.img.url for image in images],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates':{
+            'lng': place.coordinate_lng,
+            'lat': place.coordinate_lat,
+        },
+    }
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False, 'indent': 4})
