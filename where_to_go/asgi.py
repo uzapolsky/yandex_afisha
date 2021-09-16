7dcb5b4b4c73f5fdc9a0739c3adad3b70e9a0073
uzapolsky <uzapolsky@gmail.com>
2021-09-16 13:00:08 +0300

    change project name and delete django secret key

--- where_to_go/asgi.py
+++ where_to_go/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for yandex_afisha project.
+ASGI config for where_to_go project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yandex_afisha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'where_to_go.settings')
 
 application = get_asgi_application()
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,4 +1,4 @@
-"""yandex_afisha URL Configuration
+"""where_to_go URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
