4acbcecd2d35e3802c42758d90d9d64a005ae896
uzapolsky <uzapolsky@gmail.com>
2021-09-17 14:09:52 +0300

    add manage.py load_place command for upload json description files

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,36 @@
+import requests
+import uuid
+from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
+from places.models import Image, Place
+
+
+class Command(BaseCommand):
+    help = 'Load place to website using .json description file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json-description', type=str)
+
+    def handle(self, *args, **options):
+        response = requests.get(options['json-description'])
+        response.raise_for_status()
+        place_desc = response.json()
+
+        place, created = Place.objects.get_or_create(
+            title=place_desc['title'],
+            description_short=place_desc['description_short'],
+            description_long=place_desc['description_long'],
+            coordinate_lng=place_desc['coordinates']['lng'],
+            coordinate_lat=place_desc['coordinates']['lat'],
+        )
+
+        for position, img_link in enumerate(place_desc['imgs']):
+            image, created = Image.objects.get_or_create(
+                place=place,
+                position=position,
+            )
+            img = requests.get(img_link)
+            response.raise_for_status()
+            f = ContentFile(img.content)
+            img_name = str(uuid.uuid4())
+            image.img.save(img_name, f, save=True)
