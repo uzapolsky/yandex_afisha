a61a1fe41844c1b2c6062f2278f39ce3f94d06cb
alena-yudzina <lenok.ok.lol@gmail.com>
2021-09-21 13:41:41 +0300

    add verbose names and blank attributes to models

--- places/models.py
+++ places/models.py
@@ -6,10 +6,15 @@ from tinymce.models import HTMLField
 class Place(models.Model):
     title = models.CharField(
         max_length=200,
+        verbose_name='название',
     )
-    description_short = models.TextField()
+    description_short = models.TextField(
+        blank=True,
+        verbose_name='короткое описание',
+    )
     description_long = HTMLField(
         blank=True,
+        verbose_name='подробное описание',
     )
     coordinate_lng = models.DecimalField(
         max_digits=20,
@@ -23,17 +28,24 @@ class Place(models.Model):
     )
 
     def __str__(self):
-        return '{0}'.format(self.title)
+        return self.title
 
 
 class Image(models.Model):
-    position = models.PositiveSmallIntegerField(default=0)
+    position = models.PositiveSmallIntegerField(
+        default=0,
+        blank=True,
+        verbose_name='порядковый номер',
+    )
     place = models.ForeignKey(
         Place,
         related_name='images',
         on_delete=models.CASCADE,
+        verbose_name='место',
     )
-    img = models.ImageField()
+    img = models.ImageField(
+        verbose_name='изображение',
+    )
 
     def __str__(self):
         return '{0} - {1}'.format(self.place.title, self.position)
